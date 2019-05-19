16abfd2b90a8cd98417d6cda70c0d13cf899654e
Claudio Canales <ccanales@progrexion.com>
2019-05-18 23:01:18 -0600

    TwoSum added O(n) solution with using dictionary

--- 001-twosum-BF.py
+++ 001-twosum-BF.py
@@ -1,9 +1,19 @@
 class Solution(object):
-    def twoSum(self, nums, target):
+    def twoSumBF(self, nums, target):
         for i in range(len(nums)):
             for j in range(i + 1, len(nums)):
                 if nums[i] + nums[j] == target:
                     return [i , j]
+        return -1, -1
+
+    def twoSumDic(self, nums, target):
+        dic = {}
+        for i in range(len(nums)):
+            if nums[i] not in dic:
+                dic[target - nums[i]] = i
+            else:
+                return dic[nums[i]], i
+        return -1, -1
 
 solution = Solution()
-print(solution.twoSum([2,7,11,15],9))
+print(solution.twoSumDic([2,7,11,15],9))
